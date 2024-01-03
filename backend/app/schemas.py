a78fd6ebcf510d05a575b48a5ed9bd2642efead4
Maksuel Nascimento <114680477+mk-nascimento@users.noreply.github.com>
2024-01-03 20:45:48 -0300

    feat(database): Add user table and generate initial migrations
    - Implemented user table in the database
    - Generated initial migrations for the changes

---
+++ backend/app/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class UserSchema(BaseModel):
+    username: str
+    password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    is_superuser: bool
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserList(BaseModel):
+    users: list[UserPublic]
+
+
+class Message(BaseModel):
+    detail: str
