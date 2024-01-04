03b4267785e794e093f9a2c8d622e84a582e639f
Maksuel Nascimento <114680477+mk-nascimento@users.noreply.github.com>
2024-01-03 21:43:47 -0300

    refactor(settings): Restructure project - Created routers for Users and Auth; added new environment variables
    - Introduced routers for Users and Auth for improved project structure
    - Included additional environment variables

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,4 +11,4 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str]
     password: Mapped[str]
-    is_superuser: Mapped[str]
+    is_superuser: Mapped[str] = mapped_column(default=True)
