e1107e462c739e0cbc3331cbe9086d2289593617
Maksuel Nascimento <114680477+mk-nascimento@users.noreply.github.com>
2024-01-09 14:25:43 -0300

    fix(style): Adjust dynamic navbar justify content style

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { BsPersonCircle } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import Pathnames from "src/enums/Pathnames";
@@ -7,19 +7,25 @@ interface Props {
   signin?: boolean;
 }
 export default function Navbar({ signin }: Readonly<Props>) {
-  const [content, setContent] = useState<"between" | "start">("between");
+  const ref = useRef<HTMLElement>(null);
 
   useEffect(
     function () {
-      if (signin) setContent("start");
-      else setContent("between");
+      const { current: nav } = ref;
+      let dataset: DOMStringMap | undefined;
+
+      if (nav) dataset = nav.dataset;
+      else return;
+
+      if (dataset.signin) nav.className = "flex h-full w-full items-center justify-start";
+      else nav.className = "flex h-full w-full items-center justify-between";
     },
-    [signin],
+    [ref],
   );
 
   return (
     <div aria-label="navbar container" className="sticky left-0 top-0 z-[1] h-24 w-full bg-brand-100 px-8 py-4">
-      <nav className={`flex h-full w-full items-center justify-${content}`}>
+      <nav ref={ref} data-signin={signin}>
         <Link className="h-full" to={Pathnames.Homepage}>
           <img className="h-full" src="/src/assets/verzel.svg" alt="verzel-motors logo" />
         </Link>
